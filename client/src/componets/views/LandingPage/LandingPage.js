a5e70aca782a58b47428395693860bf63a1d5257
parkchanmi <mimi226kr@naver.com>
2021-12-28 22:03:47 +0900

    login

--- client/src/componets/views/LandingPage/LandingPage.js
+++ client/src/componets/views/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button } from 'reactstrap';
+//import { Button } from 'reactstrap';
+import Button from 'react-bootstrap/Button';
 import {
     BrowserRouter as Router,
     Route,
@@ -8,7 +9,15 @@ import {
 
 function LandingPage() {
     return (
-        <Link to = "/login">무료로 시작하기</Link>
+        <div>
+            <div className="float-start">
+                <Button variant="secondary" type="submit"><Link to="/">BANANA</Link></Button>
+            </div>
+            <div className="float-end">
+            <Button variant="secondary" type="submit"><Link to="/login">Login</Link></Button>
+            <Button variant="secondary" type="submit"><Link to="/login">무료로 시작하기</Link></Button>
+            </div>
+        </div>
     )
 }
 
--- client/src/componets/views/LoginPage/LoginPage.js
+++ client/src/componets/views/LoginPage/LoginPage.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function LoginPage() {
-    return (
-        <div>
-            LoginPage
-        </div>
-    )
-}
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-export default LoginPage
+import '../../../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+
+  return (
+    <>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+      </Toast>
+    </>
+  );
+};
+
+const App = () => (
+  <Container className="p-3">
+    <Container className="p-5 mb-4 bg-light rounded-3">
+      <h1 className="header">로그인</h1>
+    
+    <Form>
+  <Form.Group className="mb-3" controlId="formBasicId">
+    <Form.Control type="id" placeholder="아이디" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Control type="password" placeholder="패스워드" />
+  </Form.Group>
+  <p className="float-end">비밀번호찾기</p>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="자동 로그인" />
+  </Form.Group>
+  <div className="text-center">
+  <Button variant="primary" type="submit">
+    로그인
+  </Button>
+  </div>
+  <div className="text-center">
+      <span className="m-md-2">아직 계정이 없으신가요?</span><span className="m-md-2">회원가입</span></div>
+  
+</Form>
+    </Container>
+  </Container>
+);
+
+export default App;
