b51ea08c2316dcbaa109a5aae0e4abda87d47aa5
Erin <se.young492@gmail.com>
2022-02-12 17:19:02 +0900

    20220212_register

--- client/src/componets/views/RegisterPage/RegisterPage.js
+++ client/src/componets/views/RegisterPage/RegisterPage.js
@@ -7,38 +7,36 @@ import {
     Label
   } from 'reactstrap';
 import { Link } from "react-router-dom";
+import '../../../css/common.css';
+import '../../../css/main.css';
 
 function RegisterPage() {
     return (
-        <div className="h-100">
+        <div className="wrap col-xs-12 col-md-12 col-lg-12">
             <div className="header w-100">
-                <div className="float-start col-4">
-                    <Button className="m-3" variant="secondary" type="submit"><Link to="/">BANANA</Link></Button>
-                </div>
+                <Link to="/" className='header_link'>BANANA</Link>
             </div>
-            <div>
-                <h2>Sign In</h2>
-                    <Form className="form">
-                    <FormGroup>
-                        <Label for="exampleEmail">Username</Label>
-                        <Input
-                        type="email"
-                        name="email"
-                        id="exampleEmail"
-                        placeholder="example@example.com"
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="examplePassword">Password</Label>
-                        <Input
-                        type="password"
-                        name="password"
-                        id="examplePassword"
-                        placeholder="********"
-                        />
-                    </FormGroup>
-                    <Button>Submit</Button>
-                </Form>
+            <div className='container banna_container'>
+                <div className='register_form'>
+                    <h2 className='title'>회원가입</h2>
+                    <Form>
+                        <h3 className='sub_title'>기본정보</h3>
+                        <Input id="id" name="id" placeholder="아이디" type="text"/>                        
+                        <Input id="pwd" name="pwd" placeholder="비밀번호" type="password"/>
+                        <Input id="pwd" name="pwd" placeholder="비밀번호 확인" type="password"/>
+                        <Input id="email" name="email" placeholder="이메일" type="email"/>
+                        <h3 className='sub_title'>사업장 정보</h3>
+                        <Input id="bs_company" name="bs_company" placeholder="사업장명" type="text"/>
+                        <Input id="bs_owner_name" name="bs_owner_name" placeholder="대표자명" type="text"/>
+                        <Input id="bs_company_hp" name="bs_company_hp" placeholder="사업장 연락처" type="text"/>
+                        <Button className='regiser_btn'>가입하기</Button>
+                    </Form>
+                    <div className='login_go'>
+                        <span>이미 계정이 있으신가요?</span>
+                        <Link to="/login" className='login_link'>로그인</Link>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
