ec40a05ffc8e36f56efc7004f1895ae9e4878c9a
Erin <se.young492@gmail.com>
2022-02-20 20:47:59 +0900

    20220220_gitpage4

--- client/src/componets/views/LandingPage/LandingPage.js
+++ client/src/componets/views/LandingPage/LandingPage.js
@@ -19,16 +19,16 @@ function LandingPage() {
     return (
         <div className="h-100">
             <div className="float-start col-4">
-                <Button className="m-3 btn-light" variant="secondary" type="submit"><Link to="/">BANANA</Link></Button>
+                <Button className="m-3 btn-light" variant="secondary" type="submit"><Link to={process.env.PUBLIC_URL + '/'}>BANANA</Link></Button>
             </div>
             <div className="float-end col-8 text-end">
-                <Button className="btn-light" variant="secondary" type="submit"><Link to="/login">Login</Link></Button>
-                <Button className="m-3 btn-light" variant="secondary" type="submit"><Link to="/register">무료로 시작하기</Link></Button>
+                <Button className="btn-light" variant="secondary" type="submit"><Link to={process.env.PUBLIC_URL + '/login'}>Login</Link></Button>
+                <Button className="m-3 btn-light" variant="secondary" type="submit"><Link to={process.env.PUBLIC_URL + '/register'}>무료로 시작하기</Link></Button>
             </div>
 
             <Container className="float-none text-center h-100 flex">
                 <Container className="h-100 flex v-center h-center">
-                    <Button className="btn-light" variant="primary" type="submit"><Link to="/register">무료로 시작하기</Link></Button>
+                    <Button className="btn-light" variant="primary" type="submit"><Link to={process.env.PUBLIC_URL + '/register'}>무료로 시작하기</Link></Button>
                 </Container>
             </Container>
         </div>
